bfa94e20b8397984720bf5c95ba421442ea16140
Brian Coe <bwc126@gmail.com>
2015-11-18 13:32:10 -0500

    refactor: Optimize changePizzaSizes to query and calculate once per call, then set new widths for all pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -453,10 +453,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzaContainers = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(randomPizzaContainers[0], size);
+    var newwidth = (randomPizzaContainers[0].offsetWidth + dx) + 'px';
+    for (var i = 0; i < randomPizzaContainers.length; i++) {
+      randomPizzaContainers[i].style.width = newwidth;
     }
   }
 
